52d6c72a2b9a6f46c6278cc48ecfaf331927a25c
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-01-09 12:57:12 +0530

    landing page init

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main>
-      
-    </main>
-  )
+    <section className="w-full flex-center flex-col">
+      <h1 class="mb-4 text-5xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Discover <span className="text-black">and</span> Share
+        </span>{" "}
+        your Ideas.
+      </h1>
+      <p class="text-lg font-normal text-gray-00 lg:text-xl text-gray-400">
+        Your Gateway to Creativity Unleashed! Explore a boundless collection of
+        ingenious concepts and inventions, or ignite the spark of inspiration by
+        contributing your own groundbreaking ideas. Whether you're seeking
+        innovative solutions or eager to share your creative genius, Idea-bin is
+        the vibrant online community where imagination flourishes. Join us in
+        shaping the future, one idea at a time!
+      </p>
+    </section>
+  );
 }
