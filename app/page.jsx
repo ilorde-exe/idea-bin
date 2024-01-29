d952f11f26827632786130fbfe5e52b3f9baf6a3
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-01-29 14:32:32 +0530

    logo fixed

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,24 @@
 import Feed from "@components/Feed";
 export default function Home() {
   return (
-    <section className="w-full flex-center flex-col">
-      <h1 class="mb-4 text-5xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
-        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-          Discover <span className="text-black">and</span> Share
-        </span>
-        your Ideas.
-      </h1>
-      <p class="text-l font-normal text-gray-00 lg:text-xl text-gray-400">
-        Your Gateway to Creativity Unleashed! Explore a boundless collection of
-        ingenious concepts and inventions, or ignite the spark of inspiration by
-        contributing your own groundbreaking ideas. Whether you're seeking
-        innovative solutions or eager to share your creative genius, Idea-bin is
-        the vibrant online community where imagination flourishes. Join us in
-        shaping the future, one idea at a time!
-      </p>
-
+    <div>
+      <section className="w-full flex-center flex-col">
+        <h1 class="mb-4 text-5xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
+          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            Discover <span className="text-black">and</span> Share
+          </span>
+          your Ideas.
+        </h1>
+        <p class="text-l font-normal text-gray-00 lg:text-xl text-gray-400">
+          Your Gateway to Creativity Unleashed! Explore a boundless collection
+          of ingenious concepts and inventions, or ignite the spark of
+          inspiration by contributing your own groundbreaking ideas. Whether
+          you're seeking innovative solutions or eager to share your creative
+          genius, Idea-bin is the vibrant online community where imagination
+          flourishes. Join us in shaping the future, one idea at a time!
+        </p>
+      </section>
       <Feed />
-    </section>
+    </div>
   );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,5 @@
 "use client";
+import bulb from "@public/bulb.svg";
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
@@ -6,9 +7,15 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth";
 const Nav = () => {
   return (
-    <nav className="flex-between w-full mb-16 pt-3 bg-white">
-      <Link href="/" className="flex gap-2 flex-center ">
-        <Image></Image>
+    <nav className="flex-between w-full mb-16 pt-3 ">
+      <Link href="/" className="flex flex-center ">
+        <Image
+          src={bulb}
+          alt="IdeaBin logo"
+          width={30}
+          height={30}
+          className=" object-contain"
+        />
       </Link>
     </nav>
   );
