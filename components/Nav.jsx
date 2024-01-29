601c36a1bc557a40a97b3c3a2a46a7fad51b9c39
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-01-29 14:17:27 +0530

    nav white now

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,8 +6,10 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth";
 const Nav = () => {
   return (
-    <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/" className="flex gap-2 flex-center "></Link>
+    <nav className="flex-between w-full mb-16 pt-3 bg-white">
+      <Link href="/" className="flex gap-2 flex-center ">
+        <Image></Image>
+      </Link>
     </nav>
   );
 };
