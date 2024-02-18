11273ea6da5e8642b68d4d876c76e62ea6609287
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-02-18 19:27:32 +0530

    auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDb } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -8,10 +10,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
-    } catch (error) {}
+      await connectToDb();
+
+      //Check if a user already exists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      //if not create new user
+      if (userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 export { handler as GET, handler as POST };
