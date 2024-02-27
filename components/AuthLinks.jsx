026a809dafe3b3b20bed66f0a562f311b9ab684f
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-02-27 20:54:52 +0530

    Goodamnnn we are so backkk holy shitttt

---
+++ components/AuthLinks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthLinks = () => {
+  return <div>AuthLinks</div>;
+};
+
+export default AuthLinks;
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,13 +1,15 @@
 "use client";
 import bulb from "@public/bulb.svg";
 import Link from "next/link";
-import React from "react";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-const Nav = () => {
-  const isUserLoggedIn = true;
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog } from "@headlessui/react";
+import ThemeToggle from "./ThemeToggle";
+import AuthLinks from "./AuthLinks";
 
-  const [toggleDropdown, setToggleDropdown] = useState(false);
+const Nav = () => {
+  const [mobileMenuOpen, setmobileMenuOpen] = useState(false);
 
   return (
     <nav className="flex justify-between w-full mb-4 pt-3 h-16 shadow-inner shadow-xl bg-gray-0 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 ">
@@ -23,11 +25,91 @@ const Nav = () => {
           IdeaBin
         </p>
       </Link>
-      <div className="">
-        <Link>Homepage</Link>
-        <Link>Contact</Link>
-        <Link>About</Link>
+      <div className="hidden lg:flex lg:px-3 lg:py-1.5 lg:gap-3 lg:text-lg lg:text-white ">
+        <ThemeToggle />
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">About</Link>
+        <AuthLinks />
       </div>
+
+      <div className="flex lg:hidden">
+        <button
+          type="button"
+          className="m-2.5 -mt-1 inline-flex items-center justify-center rounded-xl p-2.5 text-white shadow-xl ring-1 ring-gray-900/5"
+          onClick={() => setmobileMenuOpen(true)}
+        >
+          <span className="sr-only">Open main menu</span>
+          <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setmobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10"></div>
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="flex flex-center -ml-2 -mt-0.5">
+              <Image
+                src={bulb}
+                alt="IdeaBin logo"
+                width={35}
+                height={35}
+                className="object-contain "
+              />
+              <p className="flex -mt-1 px-2 pt-2 text-white text-2xl font-bold font-inter">
+                IdeaBin
+              </p>
+            </Link>
+            <button
+              type="button"
+              className="-mt-3 p-2.5 text white inline-flex items-center justify-center rounded-xl text-white shadow-xl ring-1 ring-gray-900/5"
+              onClick={() => setmobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close Menu</span>
+              <XMarkIcon aria-hidden="true" className="h-6 w-6 text-white" />
+            </button>
+          </div>
+
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Link
+                  href="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-lg leading-7 text-white hover:bg-gray-400 hover:bg-clip-padding hover:backdrop-filter hover:backdrop-blur-2xl hover:bg-opacity-5"
+                >
+                  Homepage
+                </Link>
+                <Link
+                  href="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-lg leading-7 text-white hover:bg-gray-400 hover:bg-clip-padding hover:backdrop-filter hover:backdrop-blur-2xl hover:bg-opacity-5"
+                >
+                  Contact
+                </Link>
+                <Link
+                  href="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-lg leading-7 text-white hover:bg-gray-400 hover:bg-clip-padding hover:backdrop-filter hover:backdrop-blur-2xl hover:bg-opacity-5"
+                >
+                  About
+                </Link>
+
+                <div className="py-6">
+                  <Link
+                    href="/"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-lg leading-7 text-white hover:bg-gray-400 hover:bg-clip-padding hover:backdrop-filter hover:backdrop-blur-2xl hover:bg-opacity-5"
+                  >
+                    <AuthLinks />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </nav>
   );
 };
