a5719d7cf0b2db3035d083ad120cb4fc09530d75
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-03-09 12:13:44 +0530

    more stuff

---
+++ components/Card.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+function Card() {
+  return (
+    <div>
+      <div>
+        <Image src={} />
+      </div>
+      Card
+    </div>
+  );
+}
+
+export default Card;
--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Pagination from "./Pagination";
+import Card from "./Card";
+
+const s = {
+  glassmorphism:
+    "py-10 px-10 rounded-2xl shadow-inner shadow-xl bg-gray-0 bg-clip-padding backdrop-filter backdrop-blur-none lg:backdrop-blur-sm  bg-opacity-20 ",
+  title: "mb-6 text-3xl font-extrabold text-gray-900 ",
+};
 
 const CardList = () => {
   return (
-    <div>
-      CardList
+    <div className={s.glassmorphism}>
+      <h1 className={s.title}>Recent Posts</h1>
+      <div className="py-6 flex flex-col gap-6">
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Pagination />
     </div>
   );
