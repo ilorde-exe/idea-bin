025d3a67a16f968e233a38b514a5d9858954456d
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-04-14 21:05:14 +0530

    started on the write page

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -1,10 +1,37 @@
+"use client";
 import React from "react";
+import { PlusCircle } from "lucide-react";
+import { ImageUp, FileVideo2Icon, ExternalLink } from "lucide-react";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
   return (
-    <div>
-      Wri
-      <input type="text" />
+    <div className="flex flex-col gap-4">
+      <input
+        type="text"
+        placeholder="Title"
+        className=""
+        width={16}
+        height={16}
+      />
+      <div className="px-4 flex gap-2">
+        <button type="button" onClick={() => setOpen(!open)}>
+          <PlusCircle />
+        </button>
+        {open && <div></div>}
+      </div>
+      <ReactQuill
+        theme="snow"
+        value={value}
+        onChange={setValue}
+        placeholder="Let your ideas flow!"
+        className="text-black h-96 w-full"
+      />
     </div>
   );
 };
